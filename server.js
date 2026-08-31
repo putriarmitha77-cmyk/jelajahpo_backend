b1fb56bb16c70688bda70cfebf56fb9b2d183924
putriarmitha77-cmyk <putriarmitha77@gmail.com>
2026-08-31 11:12:31 +0700

    upload

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const express = require ('express');
 const app = express();
+const mysql = require ('mysql2');
 const PORT = 3001;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database JelajahPO');
+    }
+})
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Selamat Datang di JelajahPo API!')
+});
+
+//=====GET WISATA======//
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err});
+        res.json(results);
+    });
+});
+
+//======GET KATEGORI======//
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err});
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
